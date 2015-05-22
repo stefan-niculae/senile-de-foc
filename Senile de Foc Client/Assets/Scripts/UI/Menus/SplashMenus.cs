0872b8a96eb5eb7f4407c8db0d0975f30945d3dc
Stefan Niculae <stefan1niculae@gmail.com>
2015-05-22 21:50:48 +0300

    pre transition refactor

---
+++ Senile de Foc Client/Assets/Scripts/UI/Menus/SplashMenus.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashMenus : MonoBehaviour 
+{
+	static Transform screensHandle;
+
+	enum Steps { login, selection, customization, lobby };
+
+	static Steps _currentStep;
+	public static Steps currentStep
+	{
+		get { return _currentStep; }
+		set 
+		{
+			_currentStep = value;
+		}
+	}
+
+	void Awake ()
+	{
+		screensHandle = GameObject.Find ("Screens Handle").GetComponent <Transform> ();
+	}
+}
